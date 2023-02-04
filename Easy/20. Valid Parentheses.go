6ac3bcc9b82b7e2befcbc5c793458509d1776670
Herykurniawann <herykurniawansartono@gmail.com>
2023-02-05 00:27:44 +0700

    add some problem

---
+++ Easy/20. Valid Parentheses.go
@@ -0,0 +1,26 @@
+//https://leetcode.com/problems/valid-parentheses/
+package main
+
+func isValid(s string) bool {
+	stack := make([]rune, 0)
+	for _, c := range s {
+		if c == '(' || c == '[' || c == '{' {
+			stack = append(stack, c)
+		} else {
+			if len(stack) == 0 {
+				return false
+			}
+			if c == ')' && stack[len(stack)-1] != '(' {
+				return false
+			}
+			if c == ']' && stack[len(stack)-1] != '[' {
+				return false
+			}
+			if c == '}' && stack[len(stack)-1] != '{' {
+				return false
+			}
+			stack = stack[:len(stack)-1]
+		}
+	}
+	return len(stack) == 0
+}
